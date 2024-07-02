19881a3e92e097d531bbb70f68bd37d124364551
C Matteo <138587358+cmatteogr@users.noreply.github.com>
2024-07-02 16:11:38 -0500

    Video to Audio script

---
+++ src/media_transform/video_transform.py
@@ -0,0 +1,28 @@
+"""
+Video Transformations
+
+"""
+# Import libraries
+from moviepy.editor import VideoFileClip
+import os
+
+
+def extract_audio(video_filepath: str, video_audio_filepath: str):
+    """
+    Extract audio from video file
+    :param video_filepath:
+    :param video_audio_filepath:
+    :return:
+    """
+    # Validate filepaths
+    assert os.path.splitext(video_filepath)[1] == '.mp4', 'Invalid video extension, it must be mp4'
+    assert os.path.splitext(video_audio_filepath)[1] == '.mp3', 'Invalid video audio extension, it must be mp3'
+
+    # Load the video file
+    video = VideoFileClip(video_filepath)
+
+    # Extract the audio
+    audio = video.audio
+
+    # Save the audio to a file
+    audio.write_audiofile(video_audio_filepath)
---
+++ test_video_transform.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from src.media_transform.video_transform import extract_audio
+import os
+
+
+class Test(TestCase):
+    def test_extract_audio(self):
+        # Given the input arguments
+        
+        extract_audio(video_filepath, video_audio_filepath)
+
+        self.assertTrue(os.path.exists(video_audio_filepath), 'Video audio file not generated')
